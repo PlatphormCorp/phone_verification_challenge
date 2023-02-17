fe85dc11dc1abfb65092124561ecf87c0f825397
Anandha Abhay <aabhay@pm.me>
2023-02-16 21:05:53 -0600

    Adds VerificationCode.generate for spawning active codes to a phone
    Summary:
    We want to single point of entry to generating verification codes for
    specific phones so we don't do it in a bunch of different ways across
    the application.
    
    I think we have all the pieces we need now to set up our controller(s).

--- app/models/verification_code.rb
+++ app/models/verification_code.rb
@@ -4,4 +4,11 @@ class VerificationCode < ApplicationRecord
   validates :code, presence: true
 
   scope :active, -> { where("expires_at > ?", Time.now) }
+
+  def self.generate(phone)
+    code = SecureRandom.hex(3)
+    expires_at = Time.now + 5.minutes
+
+    phone.verification_codes.create(code: code, expires_at: expires_at)
+  end
 end
--- spec/models/verification_code_spec.rb
+++ spec/models/verification_code_spec.rb
@@ -7,7 +7,8 @@ RSpec.describe VerificationCode, type: :model do
   # TODO: Factories would be nice via FactoryBot
   describe "scopes" do
     describe ".active" do
-      let(:phone) { Phone.create(number: "3123079148, verification_expiration: 10.years.from_now") }
+      let(:phone) { Phone.create(number: "3123079148", verification_expiration: 10.years.from_now) }
+
       it "returns verification codes that are not expired" do
         active_code = VerificationCode.create(code: "1234", expires_at: 1.hour.from_now, phone: phone)
         inactive_code = VerificationCode.create(code: "1234", expires_at: 1.hour.ago, phone: phone)
@@ -15,6 +16,23 @@ RSpec.describe VerificationCode, type: :model do
         expect(VerificationCode.active).to include(active_code)
         expect(VerificationCode.active).to_not include(inactive_code)
       end
+    end
+  end
+
+  describe ".generate(phone)" do
+    let(:phone) { Phone.create(number: "3123079148", verification_expiration: 10.years.from_now) }
+
+    it "generates a reasonably hard to guess code that is still human input friendly" do
+      secure_code = "123456"
+      allow(SecureRandom).to receive(:hex).and_return(secure_code)
+
+      verification_code = VerificationCode.generate(phone)
+      expect(verification_code.code).to eq(secure_code)
+    end
+
+    it "sets the expiration to 5 minutes from now" do
+      verification_code = VerificationCode.generate(phone)
+      expect(verification_code.expires_at).to be_within(1.second).of(5.minutes.from_now)
     end
   end
 end
