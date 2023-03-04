5438a0375d0fe6ea4027ac32836f1338ef83444e
John Dyer <lbrapid@gmail.com>
2023-03-04 13:53:56 -0500

    Get basic functionality working

--- app/controllers/verify_phone_controller.rb
+++ app/controllers/verify_phone_controller.rb
@@ -1,5 +1,41 @@
 class VerifyPhoneController < ApplicationController
   def index
+    @phone_number = PhoneNumber.new
+  end
 
+  def code
+    @phone_number = PhoneNumber.find_by(number: phone_number_params[:number])
+    @phone_number ||= PhoneNumber.create(number: phone_number_params[:number])
+
+    if @phone_number.verified
+      flash[:notice] = 'Phone number has already been verified.'
+      return redirect_to root_path
+    end
+
+    if @phone_number.save && SmsSender.new(@phone_number).send_verification_code
+      respond_to do |format|
+        format.turbo_stream
+      end
+    else
+      render :index
+    end
+  end
+
+  def verify
+    @phone_number = PhoneNumber.find_by(number: phone_number_params[:number])
+
+    if @phone_number.validate_code(phone_number_params[:verification_code])
+      @phone_number.update_attribute(:verified, true)
+      flash[:notice] = 'Your phone number has been verified.'
+      redirect_to root_path
+    else
+      render :verify, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def phone_number_params
+    params.require(:phone_number).permit(:number, :verification_code)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "verify_phone#index"
+  root 'verify_phone#index'
+
+  post 'verification_code', to: 'verify_phone#code'
+  post 'verify_code', to: 'verify_phone#verify'
 end
