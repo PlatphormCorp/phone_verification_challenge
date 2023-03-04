f2d8776f73b66cd36930c57cd03b1e83b257e322
John Dyer <lbrapid@gmail.com>
2023-03-04 13:52:46 -0500

    Add phone number model and table

---
+++ app/models/phone_number.rb
@@ -0,0 +1,15 @@
+class PhoneNumber < ApplicationRecord
+  before_create :set_verification_code
+
+  validates :number, presence: true
+
+  def set_verification_code
+    self.verification_code = rand(10**6).to_s
+  end
+
+  def validate_code(code)
+    return true if verification_code == code
+
+    errors.add(:base, 'Incorrect verification code.') and return false
+  end
+end
